15856f3c1b5abc871e32568449785f9fd8f1c60a
Sad Onion <30446684+SadOnion@users.noreply.github.com>
2022-08-15 18:03:20 +0200

    score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,33 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using Utility.Events;
 
 namespace Wowie4
 {
-    public class Score : MonoBehaviour
-    {
-        [SerializeField] int score;
-        [SerializeField] TMPro.TextMeshProUGUI scoreText;
+	public class Score : MonoBehaviour
+	{
+		[SerializeField] int score, maxScore;
+		[SerializeField] TMPro.TextMeshProUGUI scoreText;
 
-        [Header("Events")]
-        [SerializeField] VoidEvent goodCodeEaten;
-        [SerializeField] VoidEvent neutralCodePassed;
+		[Header("Events")]
+		[SerializeField] VoidEvent goodCodeEaten;
+		[SerializeField] VoidEvent neutralCodePassed;
 
-        private void Awake()
-        {
-            Assert.IsNotNull(scoreText);
+		private void Awake()
+		{
+			Assert.IsNotNull(scoreText);
 
-            goodCodeEaten.OnEventRaised += AddScore;
-            neutralCodePassed.OnEventRaised += AddScore;
-        }
+			goodCodeEaten.OnEventRaised += AddScore;
+			neutralCodePassed.OnEventRaised += AddScore;
+		}
 
-        private void AddScore()
-        {
-            score++;
-            scoreText.text = $"Score: {score}";
-        }
+		private void AddScore()
+		{
+			score++;
+			scoreText.text = $"Functions Learned: {score}/{maxScore}";
+		}
 
-    }
+	}
 }
