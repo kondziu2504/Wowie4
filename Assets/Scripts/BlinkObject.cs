6554f472e467b02a946b9607eff195b310fecf32
Sad Onion <30446684+SadOnion@users.noreply.github.com>
2022-08-15 13:21:19 +0200

    S down arrow key indicator

---
+++ Assets/Scripts/BlinkObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wowie4
+{
+	public class BlinkObject : MonoBehaviour
+	{
+		public float blinkTime = .5f;
+		public GameObject blinkObject;
+		private bool isBlinking;
+		private Coroutine blinkCoroutine;
+		IEnumerator Blink()
+		{
+			isBlinking = true;
+			yield return new WaitForSeconds(blinkTime);
+			blinkObject.SetActive(true);
+			yield return new WaitForSeconds(blinkTime);
+			blinkObject.SetActive(false);
+			blinkCoroutine = StartCoroutine(Blink());
+		}
+
+		public void StartBlinking()
+		{
+			if (isBlinking == false)
+			{
+				blinkCoroutine = StartCoroutine(Blink());
+			}
+		}
+		public void StopBlinking()
+		{
+			isBlinking = false;
+			StopCoroutine(blinkCoroutine);
+			blinkObject.SetActive(false);
+		}
+	}
+}
--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,95 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Wowie4
 {
-    [RequireComponent(typeof(Rigidbody2D))]
-    public class Elevator : MonoBehaviour
-    {
-        [SerializeField] float maxHeight = 2f;
-        [SerializeField] float descendingSpeed = 1f;
-        [SerializeField] float ascendingSpeed = 1f;
-        [SerializeField] float descendBoostMultiplier = 2f;
-        [SerializeField] RuntimeGameData runtimeGameData;
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class Elevator : MonoBehaviour
+	{
+		[SerializeField] float maxHeight = 2f;
+		[SerializeField] float descendingSpeed = 1f;
+		[SerializeField] float ascendingSpeed = 1f;
+		[SerializeField] float descendBoostMultiplier = 2f;
+		[SerializeField] RuntimeGameData runtimeGameData;
+		[SerializeField] BlinkObject blinkObject;
 
-        private float originalHeight;
+		private float originalHeight;
 
-        private float BottomHeight => Application.isPlaying ? originalHeight : transform.position.y;
-        private float TopHeight => BottomHeight + maxHeight;
+		private float BottomHeight => Application.isPlaying ? originalHeight : transform.position.y;
+		private float TopHeight => BottomHeight + maxHeight;
 
-        private bool descending = false;
+		private bool descending = false;
 
-        protected Controls controls;
+		protected Controls controls;
 
-        private new Rigidbody2D rigidbody;
+		private new Rigidbody2D rigidbody;
 
-        private void Awake()
-        {
-            originalHeight = transform.position.y;
-            rigidbody = GetComponent<Rigidbody2D>();
-        }
+		private void Awake()
+		{
+			originalHeight = transform.position.y;
+			rigidbody = GetComponent<Rigidbody2D>();
+		}
 
-        private void OnEnable()
-        {
-            controls = new Controls();
-            controls.Enable();
-        }
+		private void OnEnable()
+		{
+			controls = new Controls();
+			controls.Enable();
+		}
 
-        private void OnDisable()
-        {
-            controls.Disable();
-        }
+		private void OnDisable()
+		{
+			controls.Disable();
+		}
 
-        private void FixedUpdate()
-        {
-            if (!descending)
-                Ascend();
-        }
+		private void FixedUpdate()
+		{
+			if (!descending)
+				Ascend();
+		}
 
-        private void Update()
-        {
-            runtimeGameData.Energy = Mathf.InverseLerp(TopHeight, BottomHeight, transform.position.y);
-        }
+		private void Update()
+		{
+			runtimeGameData.Energy = Mathf.InverseLerp(TopHeight, BottomHeight, transform.position.y);
+		}
 
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.green;
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.green;
 
-            Gizmos.DrawLine(
-                new Vector3(transform.position.x, BottomHeight),
-                new Vector3(transform.position.x, TopHeight));
-        }
+			Gizmos.DrawLine(
+				new Vector3(transform.position.x, BottomHeight),
+				new Vector3(transform.position.x, TopHeight));
+		}
 
-        private void OnTriggerStay2D(Collider2D collision)
-        {
-            if (collision.transform.position.y > transform.position.y)
-            {
-                Descend();
-                descending = true;
-            }
-        }
+		private void OnTriggerStay2D(Collider2D collision)
+		{
+			if (collision.transform.position.y > transform.position.y)
+			{
+				Descend();
+				descending = true;
+				blinkObject.StartBlinking();
+			}
+		}
 
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            descending = false;
-        }
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			descending = false;
+			blinkObject.StopBlinking();
+		}
 
-        private void Descend()
-        {
-            var change = Vector2.down * descendingSpeed * Time.deltaTime;
-            if (controls.Player.ElevatorDown.ReadValue<float>() != 0)
-                change *= descendBoostMultiplier;
-            var finalPos = new Vector2(rigidbody.position.x, Mathf.Clamp(rigidbody.position.y + change.y, BottomHeight, TopHeight));
-            rigidbody.MovePosition(finalPos);
-        }
+		private void Descend()
+		{
+			var change = Vector2.down * descendingSpeed * Time.deltaTime;
+			if (controls.Player.ElevatorDown.ReadValue<float>() != 0)
+				change *= descendBoostMultiplier;
+			var finalPos = new Vector2(rigidbody.position.x, Mathf.Clamp(rigidbody.position.y + change.y, BottomHeight, TopHeight));
+			rigidbody.MovePosition(finalPos);
+		}
 
-        private void Ascend()
-        {
-            var change = Vector2.up * ascendingSpeed * Time.deltaTime;
-            if (runtimeGameData.ShieldActive)
-                change *= 1.5f;
-            rigidbody.MovePosition(new Vector3(rigidbody.position.x, Mathf.Clamp(rigidbody.position.y + change.y, BottomHeight, TopHeight)));
-        }
-    }
+		private void Ascend()
+		{
+			var change = Vector2.up * ascendingSpeed * Time.deltaTime;
+			if (runtimeGameData.ShieldActive)
+				change *= 1.5f;
+			rigidbody.MovePosition(new Vector3(rigidbody.position.x, Mathf.Clamp(rigidbody.position.y + change.y, BottomHeight, TopHeight)));
+		}
+	}
 }
